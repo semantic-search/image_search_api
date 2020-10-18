f98fe290e0d8fc0cb5e4ee1ccbda6ceed3884611
jainal09 <jainal09gosalia@gmail.com>
2020-10-19 04:39:36 +0530

    finished implementing image search engine

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
 from db_models.mongo_setup import global_init
 from db_models.models.cache_model import Cache
 from db_models.models.feature_model import Features
+from db_models.models.file_model import FilesModel
 import pickle
 import numpy as np
 from fastapi import FastAPI, File, UploadFile, Form
 from feature_extractor import FeatureExtractor
 import uuid
+import base64
+
 
 fe = FeatureExtractor()
 app = FastAPI()
 global_init()
 features = list()
-img_ids = list()
+feature_ids = list()
 doc_ids = list()
 
 
 for feature_obj in Features.objects:
     features.append(pickle.loads(feature_obj.feature))
-    img_ids.append(feature_obj.file)
-    doc_ids.append(feature_obj.document)
+    feature_ids.append(feature_obj)
+    doc_ids.append(feature_obj.document.id)
 
 
 @app.post("/search/")
-def search(file: UploadFile = File(...)):
+def search(file: UploadFile = File(...), skip: int = 0):
+    skip = skip * 10
+    limit = skip + 10
     file_name = str(uuid.uuid4()) + file.filename
     with open(file_name, 'wb') as f:
         f.write(file.file.read())
@@ -30,18 +35,21 @@ def search(file: UploadFile = File(...)):
     # Run search
     query = fe.extract(file_name)
     dists = np.linalg.norm(features - query, axis=1)  # L2 distances to features
-    ids = np.argsort(dists)[:30]  # Top 30 results
-    files_ids = list()
+    np_array = np.argsort(dists)
+    ids = np_array[skip:limit]  # Top 30 results
+    files_b_64 = list()
     scores = list()
-    document_ids = list()
+    documents = list()
     for id in ids:
-        scores.append(dists[id])
-        files_ids.append(img_ids[id])
-        document_ids.append(doc_ids[id])
+        scores.append(1 - float(dists[id]))
+        files_b_64.append(base64.b64encode(feature_ids[id].file.read()))
+        cache_obj = Cache.objects.get(id=doc_ids[id])
+        documents.append(str(cache_obj.file_name))
     final = {
-        "document": document_ids,
+        "total_pages": len(np_array),
+        "document": documents,
         "scores": scores,
-        "files": files_ids
+        "files": files_b_64
     }
     return final
 
